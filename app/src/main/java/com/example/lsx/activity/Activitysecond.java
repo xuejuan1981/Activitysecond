b44a1639b913262d446e2fe030fb94d810eb925e
hxj <hxj@163.com>
2016-07-22 10:41:58 +0800

    second

---
+++ app/src/main/java/com/example/lsx/activity/Activitysecond.java
@@ -0,0 +1,9 @@
+package com.example.lsx.activity;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by lsx on 2016/7/22.
+ */
+public class Activitysecond extends AppCompatActivity {
+}
